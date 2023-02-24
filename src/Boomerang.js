0315596ee3313b3fc89e420f58ab4c468a613a4d
merkuspb <msemenova21@gmail.com>
2023-02-24 14:21:04 +0300

    created boomerang

---
+++ src/Boomerang.js
@@ -0,0 +1,31 @@
+class Boomerang {
+  constructor() {
+    this.skin = '🦯';
+    this.position = -1;
+  }
+
+  fly(hero) {
+    if (this.position < hero.position + 5) {
+      this.moveRight();
+    } else {
+      this.moveLeft();
+      this.moveLeft();
+      this.moveLeft();
+      this.moveLeft();
+    }
+    if (this.position === hero.position) {
+      this.position = '?';
+    }
+  }
+
+  moveLeft() {
+    // Идём влево.
+    this.position -= 1;
+  }
+
+  moveRight() {
+    // Идём вправо.
+    this.position += 1;
+  }
+}
+module.exports = Boomerang;
